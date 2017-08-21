8f6976ce78b9558022129669ea0e40ead01d7168
GlennGeelen <g.geelen@defacto.nl>
2017-08-21 16:51:01 +0200

    Update phoenix to 1.3.0

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Darkstone.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Darkstone"
   end
 end
